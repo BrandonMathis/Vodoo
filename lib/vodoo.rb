b9f94ee387035506e74a8a4601b259634071105c
BrandonMathis <Brandon@KeysetTS.com>
2011-09-29 00:13:06 -0400

    Added remove task and gave tasks SHA identifiers

--- lib/vodoo.rb
+++ lib/vodoo.rb
@@ -11,7 +11,23 @@ module Vodoo
     def add_task(time, content)
       tasks = all_tasks
       time = Chronic.parse(time).to_s
-      tasks[time] = content
+      sha = Digest::SHA1.hexdigest content
+      tasks[sha] = {}
+      tasks[sha][:content] = content
+      tasks[sha][:created] = time
+      File.open(task_file, "w") do |f|
+        f.write(tasks.to_yaml)
+      end
+      return sha
+    end
+
+    def remove_task(sha)
+      tasks = all_tasks
+      tasks.delete sha
+    end
+
+    private
+    def publish_tasks(tasks)
       File.open(task_file, "w") do |f|
         f.write(tasks.to_yaml)
       end
--- spec/vodoo/tasks_spec.rb
+++ spec/vodoo/tasks_spec.rb
@@ -9,21 +9,25 @@ describe Vodoo do
       Vodoo.task_file = @file
     end
     it "should should list all tasks in task.yml" do
-      Vodoo.all_tasks.should include("one" => "One")
+      Vodoo.all_tasks.should include("fe05bcdcdc4928012781a5f1a2a77cbb5398e106" => {"content" => "One"})
     end
   end
   describe "add_task" do
     before do
       @file = File.join("spec", "fixtures", "new_tasks.yml")
       Vodoo.task_file = @file
+      @sha = Vodoo.add_task("today at midnight", "Make this spec pass")
     end
     it "should create a task in the task.yml" do
-      time =  Time.now.to_s
-      Vodoo.add_task(time, "Make this spec pass")
-      Vodoo.all_tasks.should include(time => "Make this spec pass")
+      Vodoo.all_tasks[@sha].should include( :content => "Make this spec pass" )
     end
     after do
       File.delete(@file) if File.exist? @file
+    end
+    describe "remove_task" do
+      it "should removed task from the yml" do
+        Vodoo.remove_task @sha
+      end
     end
   end
 end
